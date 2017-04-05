6df2f9a6d17ce43be2f4e6bf4f7ef4b4c5f408ea
Thor Erik Lie <thor@thorerik.com>
2017-04-05 11:13:35 +0200

    Lecture 20

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -42,12 +42,10 @@ defmodule Cards do
   end
 
   def load(filename) do
-    {status, binary} = File.read(filename)
-
-    case status do
-      :ok -> :erlang.binary_to_term binary
-      :error -> "File does not exist"
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, :enoent} -> "File does not exist"
+      {:error, err} -> "Unkown error: #{err}"
     end
   end
-
 end
