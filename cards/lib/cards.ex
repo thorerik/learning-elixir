7b16ecf66dcd2a72db56e17a2b9b7802c142cf61
Thor Erik Lie <thor@thorerik.com>
2017-04-05 11:22:24 +0200

    Lecture 22

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -48,4 +48,10 @@ defmodule Cards do
       {:error, err} -> "Unkown error: #{err}"
     end
   end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size) 
+  end
 end
