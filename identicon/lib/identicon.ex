ef36916448d3d8e8b8a8f2269b45aa2f8a48cf78
Thor Erik Lie <thor@thorerik.com>
2017-04-06 05:36:45 +0200

    Lecture 37

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,16 +3,21 @@ defmodule Identicon do
   Documentation for Identicon.
   """
 
+  def main(input) do
+    input
+    |> hash_input
+  end
+
   @doc """
-  Hello world.
+  Hashes the input using the MD5 algorithm
 
   ## Examples
 
-      iex> Identicon.hello
-      :world
-
+      iex> Identicon.main("meow")
+      [74, 75, 228, 12, 150, 172, 99, 20, 233, 29, 147, 243, 128, 67, 166, 52]
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
