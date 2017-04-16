a8abfe178839c09cbb9819591755c6a3fdcf2a5b
Thor Erik Lie <thor@thorerik.com>
2017-04-16 14:19:00 +0200

    Lecture 41

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -6,12 +6,21 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
-    |> pick_color
+    |> pick_colour
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r,g,b|_]} = image
-    [r,g,b]
+  @doc """
+  Picks the colour off the hash and adds it to our struct
+
+  ## Examples
+
+      iex> imgStruct = Identicon.hash_input ("meow")
+      iex> Identicon.pick_colour(imgStruct)
+      %Identicon.Image{colour: {74, 75, 228},
+      hex: [74, 75, 228, 12, 150, 172, 99, 20, 233, 29, 147, 243, 128, 67, 166, 52]}
+  """
+  def pick_colour(%Identicon.Image{hex: [r,g,b|_]} = image) do
+    %Identicon.Image{image | colour: {r, g, b}}
   end
 
   @doc """
