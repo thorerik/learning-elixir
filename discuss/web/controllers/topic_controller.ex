942f40ecf1b51188c6b5cb026d65b8c7c3576778
Thor Erik Lie <thor@thorerik.com>
2017-04-27 17:14:49 +0200

    Lecture 73

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -1,6 +1,9 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
+  alias Discuss.Topic
+
   def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
   end
 end
