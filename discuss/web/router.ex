13bcdacd26ab8351b3838e8b3faee828c166579d
Thor Erik Lie <thor@thorerik.com>
2017-06-03 14:49:10 +0200

    Lecture 96

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -19,6 +19,13 @@ defmodule Discuss.Router do
     resources "/", TopicController
   end
 
+  scope "/auth", Discuss do
+    pipe_through :browser
+
+    get "/:provider", Authcontroller, :request
+    get "/:provider/callback", Authcontroller, :callback
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", Discuss do
   #   pipe_through :api
