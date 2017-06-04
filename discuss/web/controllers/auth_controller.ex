d34d7930ec50b5f102d0a17120df01a1d0602b7f
Thor Erik Lie <thor@thorerik.com>
2017-06-04 09:02:54 +0200

    Lecture 103

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -12,7 +12,17 @@ defmodule Discuss.Authcontroller do
   end
 
   defp signin(conn, changeset) do
-    insert_or_update_user(changeset)
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
   end
 
   defp insert_or_update_user(changeset) do
